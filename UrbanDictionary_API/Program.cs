8d0ff97a9b67336feb212a1bc853ff33671c2052
LalPotRash <daniil.kanev1994@gmail.com>
2021-06-12 17:17:14 +0300

    Добавьте файлы проекта.

---
+++ UrbanDictionary_API/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace UrbanDictionaryAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = Console.ReadLine();
+            var url = $"http://api.urbandictionary.com/v0/define?term={query}";
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var militaryBase = JsonConvert.DeserializeObject<Root>(result);
+                Console.WriteLine(militaryBase.list[0].definition);
+            }
+        }
+    }
+}
